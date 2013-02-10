22a075cf17c2925f487dc30f46476369612c8c20
singh_dd93 <singh_dd93@yahoo.in>
2013-02-11 01:27:09 +0530

    Build with linaro 4.7.3 & fixed dependencies
    Change-Id: I2505d803569b74d95712e2debf1e6a7c41ccc914

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,8 +2,8 @@
 -include vendor/semc/smultron/BoardConfigVendor.mk
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := cyanogen_smultron_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+TARGET_KERNEL_CONFIG := pa_smultron_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7.3
 
 TARGET_RECOVERY_INITRC := device/semc/smultron/recovery/init.rc
 
